60ecbee28aa117bcc13f6bfa568417c256e88986
Johnathan R. Burgess <ryanburgess173@outlook.com>
2019-12-22 19:36:18 -0500

    1

---
+++ learningUnitTest/Customer.py
@@ -0,0 +1,8 @@
+def __init__(firstName, lastName, phone, email, ssn, address):
+    self.firstName = firstName
+    self.lastName = lastName
+    self.phone = phone
+    self.email = email
+    self.ssn = ssn
+    self.address = address
+    
