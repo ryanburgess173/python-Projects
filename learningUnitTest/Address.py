1202550b6e5367e4c0829890466d8737c5719c2c
Johnathan R. Burgess <ryanburgess173@outlook.com>
2019-12-22 20:05:26 -0500

    2

---
+++ learningUnitTest/Address.py
@@ -0,0 +1,8 @@
+class Address:
+
+    def __init__(self, street_address, apt_number, city, state, zipcode):
+        self.street_address = street_address
+        self.apt_number = apt_number
+        self.city = city
+        self.state = state
+        self.zipcode = zipcode
--- learningUnitTest/Customer.py
+++ learningUnitTest/Customer.py
@@ -1,8 +1,9 @@
-def __init__(firstName, lastName, phone, email, ssn, address):
-    self.firstName = firstName
-    self.lastName = lastName
-    self.phone = phone
-    self.email = email
-    self.ssn = ssn
-    self.address = address
-    
+class Customer:
+
+    def __init__(self, firstName, lastName, phone, email, ssn, address):
+        self.firstName = firstName
+        self.lastName = lastName
+        self.phone = phone
+        self.email = email
+        self.ssn = ssn
+        self.address = address
