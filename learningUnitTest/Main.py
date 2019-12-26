9e69494577484eb89ed51019bbb17a43d09e6867
Johnathan R. Burgess <ryanburgess173@outlook.com>
2019-12-25 22:01:56 -0500

    got my classes working

--- learningUnitTest/Main.py
+++ learningUnitTest/Main.py
@@ -5,7 +5,7 @@ from Savings import Savings
 
 
 def main():
-    address = Address("1832 Billy T. Trail", "", "Mebane", "NC", "27302")
+    address = Address("1900 Travis Ln", "", "Mebane", "NC", "27302")
     customer = Customer("Johnathan", "Burgess", "(336) 508-8174",
                         "ryanburgess173@outlook.com", "000-00-0000", address)
     savings_account = Savings(customer, 8000, 4.5, 6)
