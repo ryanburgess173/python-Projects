c4892fd158c6913eaf5150e90b0675f5ff419bb6
Johnathan R. Burgess <ryanburgess173@outlook.com>
2020-02-01 18:48:04 -0500

    trying the @property functionality in Customer class

--- mysql_database_stuff/orderManagement/Customer.py
+++ mysql_database_stuff/orderManagement/Customer.py
@@ -1,13 +1,5 @@
 class Customer:
 
-    __firstName = None
-    __lastName = None
-    __homePhone = None
-    __cellPhone = None
-    __address = None
-    __email = None
-    __customerProfile = None
-
     def __int__(self, fName, lName, home, cell, address, email, cP):
         self.__firstName = fName
         self.__lastName = lName
@@ -17,26 +9,14 @@ class Customer:
         self.__email = email
         self.__customerProfile = cP
 
-    def setFirstName(self, fName):
-        self.__firstName = fName
-
-    def getFirstName(self):
+    @property
+    def firstName(self):
         return self.__firstName
 
-    def setLastName(self, lName):
-        self.__lastName = lName
+    @firstName.setter
+    def firstName(self, fName):
+        self.__firstName = fName
 
-    def getLastName(self):
-        return self.__lastName
-
-    def setHomePhone(self, homePhone):
-        self.__homePhone = homePhone
-
-    def getHomePhone(self):
-        return self.__homePhone
-
-    def setCellPhone(self, cellPhone):
-        self.__cellPhone = cellPhone
-
-    def getCellPhone(self):
-        return self.__cellPhone
+    @firstName.deleter
+    def firstName(self):
+        del self.__firstName
